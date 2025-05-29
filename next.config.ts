7890b92080c165b1ce5acea28764bfed314ec8d9
Adnan-The-Coder <adnanali23189@gmail.com>
2025-05-29 21:13:36 +0530

    feat: Add OpenNext Based Worker Deployment setup

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,12 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    domains:['lh3.googleusercontent.com']
+  images: {
+    domains: ['lh3.googleusercontent.com']
   }
 };
 
-async function setup() {
-  if (process.env.NODE_ENV === 'development') {
-    await setupDevPlatform();
-  }
-}
-setup();
-
 export default nextConfig;
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
