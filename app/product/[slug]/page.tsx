cc729d59c143673862a3a7980f38e4eca8f6e677
Adnan-The-Coder <adnanali23189@gmail.com>
2025-06-03 12:39:32 +0530

    rm edge runtime

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable no-unused-vars */
 import CompletePage from "@/components/productsPage/CompletePage";
 
-export const runtime = 'edge';
-
 export default async function Page({
   params,
 }: {
