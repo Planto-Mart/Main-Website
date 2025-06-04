65f765aa4da4b6e2c24c4e5c50f47150fdb60f15
Adnan-The-Coder <adnanali23189@gmail.com>
2025-06-04 07:20:46 +0530

    revert: edge runtime in dynamic page

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import CompletePage from "@/components/productsPage/CompletePage";
 
+export const runtime = 'edge';
+
 export default async function Page({
   params,
 }: {
